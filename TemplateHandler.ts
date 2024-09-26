09384b6fb598fb1ee81784e86bd35db2ec605f36
barak amiel <barak307@gmail.com>
2024-09-27 00:36:55 +0300

    add one of properies

--- TemplateHandler.ts
+++ TemplateHandler.ts
@@ -6,6 +6,7 @@ export interface PropertyData {
 export interface BuildData {
 	content: string;
 	requiredProperties?: string | string[];
+	oneOfProperties?: string | string[];
 }
 export class TemplateHandler {
 	private name: string;
@@ -22,9 +23,15 @@ export class TemplateHandler {
 		const propertyNames = new Set(properties.map((prop) => prop.name));
 		return this.buildData.reduce((acc, step) => {
 			const reqArray = _.toArray(step.requiredProperties ?? []);
-			if (reqArray.every((required) => propertyNames.has(required))) {
-				return acc + step.content;
-			}
+			const anyArray = _.toArray(step.oneOfProperties ?? []);
+			const requiredPresent = reqArray.every((required) =>
+				propertyNames.has(required)
+			);
+			const oneOfPresent =
+				!anyArray.length ||
+				anyArray.some((required) => propertyNames.has(required));
+
+			if (requiredPresent && oneOfPresent) return acc + step.content;
 			return acc;
 		}, "");
 	}
