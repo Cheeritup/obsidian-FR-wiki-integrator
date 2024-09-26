a9db97332dd64747f6228d49b0f8aedc6da401e4
Cheeritup <79808894+Cheeritup@users.noreply.github.com>
2024-09-26 15:34:01 +0300

    Added propertyData as an output to fixProperties

--- Project.ts
+++ Project.ts
@@ -1,5 +1,8 @@
-//const fs = require("fs");
-//const example = fs.readFileSync("./example.txt").toString();
+const fs = require("fs");
+
+import { PropertyData } from "TemplateHandler";
+
+const example = fs.readFileSync("./example.txt").toString();
 type CaptureValue = string;
 //text cleaner: remove tags
 function fixTags(article: string) {
@@ -17,7 +20,7 @@ function fixTags(article: string) {
 }
 
 //curly braces fixer (quote fixer,percentage table) !!unfinished!!
-/** @param {string} article */
+//TODO: return template name
 function fixCurlyBraces(article: string) {
 	const regexp = /{{(?<name>[/a-zA-Z0-9]*)(?<content>[^\n]*)}}\n/g;
 	return (
@@ -28,20 +31,22 @@ function fixCurlyBraces(article: string) {
 			.replace(/^}}$\n/m, "")
 	);
 }
-/** @returns {string} */
-
 const unsupportedProperties = ["allignment"];
 const unsupportedPropertyValues = ["yes", "no"];
-// removes empty properties and reformats properties into "name:value" (and corrosponding multivalue lines).
-/** @param {string} article */
-function fixProperties(article: string) {
+// removes empty properties and reformats properties into "name:value" (result)
+// returns an array with PropertyData
+function fixProperties(article: string): {
+	result: string;
+	propertyData: PropertyData[];
+} {
 	const regexp = /^\|\s(?<name>.*?)\s+=\s(?<value>.*?)\s*$\n/gm;
 	const match = article.match(regexp);
 	if (!match) {
-		return article;
+		return { result: article, propertyData: [] };
 	}
 	const totalMatches = match.length;
 	let matchIndex = 0;
+	const propertyData: PropertyData[] = [];
 	let result = article.replace(
 		regexp,
 		function replacer(match, name: CaptureValue, value: CaptureValue) {
@@ -57,6 +62,8 @@ function fixProperties(article: string) {
 				}
 				return result;
 			}
+			let currentPair: PropertyData = { name, value };
+			propertyData.push(currentPair);
 			const value_split = value.split(", ");
 			if (value_split.length === 1) {
 				let result = `${name}: ${value}\n`;
@@ -75,23 +82,23 @@ function fixProperties(article: string) {
 			return result;
 		}
 	);
-	return `---\nobsidianUIMode: preview\n${result}`;
+	result = `---\nobsidianUIMode: preview\n${result}`;
+	return {
+		result,
+		propertyData,
+	};
 }
-/** @returns {string} */
 
 // fixes heading formating, i.e =content= -> #content
-/** @param {string} article */
 function fixHeadings(article: string) {
 	const regexp = /(?<start>^=+)(?<content>.+?)(?:=+$)/gm;
 	return article.replaceAll(regexp, function replacer(match, start, content) {
 		return `${start.replaceAll("=", "#")} ${content}`;
 	});
 }
-/** @returns {string} */
 
 // Makes links link to the Forgotten Realm wiki
 //consider making a clause that checks if there is a local file in the vault with the same name and if so, skips this step.
-/** @param {string} article */
 function fixLinks(article: string) {
 	const regexp = /\[\[(?!frw:)/g;
 	return article.replaceAll(regexp, function replacer(match) {
@@ -103,9 +110,11 @@ export const parseArticle = function (article: string) {
 	let result = article.replace(/\r/g, "");
 	result = fixTags(result);
 	result = fixCurlyBraces(result);
-	result = fixProperties(result);
+	result = fixProperties(result).result;
+	console.log(fixProperties(example).propertyData);
 	result = fixHeadings(result);
 	result = fixLinks(result);
 	return result;
 };
 //console.log(parseArticle(example));
+parseArticle(example);
