2b51609230c5b830787bab7f4fd37b0acbab4300
Cheeritup <79808894+Cheeritup@users.noreply.github.com>
2024-10-09 20:00:23 +0300

    final cleanup for BETA release

--- main.ts
+++ main.ts
@@ -25,51 +25,23 @@ export default class FRWIntegrationPlugin extends Plugin {
 	settings: FRWIntegrationPluginSettings;
 
 	async onload() {
-		new Notice("FRW Integration Plugin starting up.");
 		// Quicklinks prefix check
 		const quickLinksSupport = await ensureQuickLinksSupport(this.app);
 		await this.loadSettings();
-		this.addRibbonIcon("dice", "Test button", () => {
-			const result = "test";
-			new Notice(result);
-		});
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
 			"dice",
-			"Sample Plugin",
+			"FRW Integrator Test",
 			(evt: MouseEvent) => {
 				// Called when the user clicks the icon.
-				new Notice("This ensures the plugin is loaded");
+				new Notice("This ensures the plugin is loaded for debugging.");
 			}
 		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
 		// Actual function, integrates an article's source code into the editor
 		this.addCommand({
 			id: "frw-integration-command",
-			name: "FRW Integration Command",
+			name: "Forgotten Realms Wiki Article Fix(selection)",
+
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				try {
 					//Replacement of text:
@@ -90,26 +62,6 @@ export default class FRWIntegrationPlugin extends Plugin {
 					if (error instanceof Error) {
 						console.error(`${error.name}: ${error.message}`);
 					} else console.error(error);
-				}
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			},
 		});
@@ -174,8 +126,8 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
+			.setName("Add Category Support")
+			.setDesc("Manually add support for new categories. Yet to be implemented fully.")
 			.addText((text) =>
 				text
 					.setPlaceholder("Enter your secret")
