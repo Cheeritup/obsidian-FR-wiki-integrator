f86717da01a0d0d5c3cae477ff0da6b642227596
Cheeritup <79808894+Cheeritup@users.noreply.github.com>
2024-10-10 19:04:03 +0300

    infobox name fix

--- TemplateBuilder/InfoboxTitleTemplateSegment.ts
+++ TemplateBuilder/InfoboxTitleTemplateSegment.ts
@@ -2,6 +2,6 @@ import { ConstantTemplateSegment } from "./ConstantTemplateSegment";
 
 export class InfoboxTitleTemplateSegment extends ConstantTemplateSegment {
 	constructor() {
-		super("> [!infobox]\n> ###### **`=this.file.name`**\n");
+		super("> [!infobox]\n> ###### **`=this.name`**\n");
 	}
 }
